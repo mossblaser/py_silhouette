a1870147e81dc5c984138cb8660917573a21cf03
Jonathan Heathcote <mail@jhnet.co.uk>
2019-01-14 19:17:55 +0000

    Rename project and add setup.py

--- plotter/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-"""
-A (possibly poorly designed) library for controlling plotters.
-"""
-
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+with open("pysilhouette/version.py", "r") as f:
+    exec(f.read())
+
+setup(
+    name="pysilhouette",
+    version=__version__,
+    packages=find_packages(),
+
+    # Metadata for PyPi
+    url="https://github.com/mossblaser/pysilhouette",
+    author="Jonathan Heathcote",
+    description="A USB driver and Python API to control the Silhouette Portrait plotter.",
+    license="LGPLv3",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+
+        "Intended Audience :: Developers",
+
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+    ],
+    keywords="plotter cutter driver usb silhouette",
+
+    # Requirements
+    install_requires=["pyusb"],
+)
